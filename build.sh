9ff501a4153047e911d04c0305a21fd02190d528
Anton <65621302+antonandrosov338@users.noreply.github.com>
2020-12-26 15:01:47 +0300

    Add files via upload

--- build.sh
+++ build.sh
@@ -10,17 +10,17 @@ ANDROID=$ANDROID_HOME/platforms/android-30/android.jar
 mkdir -p bin/res bin/obj bin/apk bin/dex
 
 #BUILD
-aapt package -f -m -J bin/res/ -S $RESOURCES -M $MANIFEST -I $ANDROID 
+aapt package -f -m -J bin/res/ -S $RESOURCES -M $MANIFEST -I $ANDROID && \
 
 javac -source 1.8 -target 1.8 -bootclasspath "${JAVA_HOME}/jre/lib/rt.jar" \
--classpath $ANDROID -d bin/obj/ bin/res/$PACKAGE/R.java $MAIN 
+-classpath $ANDROID -d bin/obj/ bin/res/$PACKAGE/R.java $MAIN && \
 
-dx --dex --output=bin/dex/classes.dex bin/obj/ 
+dx --dex --output=bin/dex/classes.dex bin/obj/ && \
 
 aapt package -f -M $MANIFEST -S $RESOURCES/ -I $ANDROID \
--F bin/apk/build.unsigned.apk bin/dex/
+-F bin/apk/build.unsigned.apk bin/dex/ && \
 
-zipalign -f -p 4 bin/apk/build.unsigned.apk bin/apk/build.align.apk
+zipalign -f -p 4 bin/apk/build.unsigned.apk bin/apk/build.align.apk && \
 
 if ! [ -f bin/keystore.jks ]; then
 	keytool -genkeypair -keystore bin/keystore.jks -alias androidKey \
@@ -28,13 +28,13 @@ if ! [ -f bin/keystore.jks ]; then
 fi
 
 apksigner sign --ks bin/keystore.jks --ks-key-alias androidKey --ks-pass pass:android \
---key-pass pass:android --out bin/apk/build.apk bin/apk/build.align.apk 
+--key-pass pass:android --out bin/apk/build.apk bin/apk/build.align.apk && \
 
 #INSTALL
 PACKAGE=`echo $PACKAGE | tr '/' '.'`
 
-adb devices
-adb install -r bin/apk/build.apk
-adb shell am start -a -S $PACKAGE/.MainActivity
+adb devices && \
+adb install -r bin/apk/build.apk && \
+adb shell am start -a -S $PACKAGE/.MainActivity && \
 
 exit 0
