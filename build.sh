7c6fc73161879a883d37761c234f15461830ffe8
Anton <65621302+antonandrosov338@users.noreply.github.com>
2020-12-26 15:38:42 +0300

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 #CONFIGURATION
+LIBS=libs
 PACKAGE=ru/notecode/application
-RESOURCES=src/java/$PACKAGE/res/
-MAIN=src/java/$PACKAGE/MainActivity.java
+FILES=src/java/$PACKAGE
+RESOURCES=$FILES/res/
 
-MANIFEST=src/java/$PACKAGE/AndroidManifest.xml
+MANIFEST=$FILES/AndroidManifest.xml
 ANDROID=$ANDROID_HOME/platforms/android-30/android.jar
 
 mkdir -p bin/res bin/obj bin/apk bin/dex
@@ -12,26 +13,27 @@ mkdir -p bin/res bin/obj bin/apk bin/dex
 #BUILD
 aapt package -f -m -J bin/res/ -S $RESOURCES -M $MANIFEST -I $ANDROID && \
 
-javac -source 1.8 -target 1.8 -bootclasspath "${JAVA_HOME}/jre/lib/rt.jar" \
--classpath $ANDROID -d bin/obj/ bin/res/$PACKAGE/R.java $MAIN && \
+javac -source 1.8 -target 1.8 -bootclasspath $JAVA_HOME/jre/lib/rt.jar \
+-classpath $ANDROID -d bin/obj/ bin/res/$PACKAGE/R.java $FILES/*.java && \
 
 dx --dex --output=bin/dex/classes.dex bin/obj/ && \
 
+#CREATE APKS
 aapt package -f -M $MANIFEST -S $RESOURCES/ -I $ANDROID \
 -F bin/apk/build.unsigned.apk bin/dex/ && \
 
 zipalign -f -p 4 bin/apk/build.unsigned.apk bin/apk/build.align.apk && \
 
-if ! [ -f bin/keystore.jks ]; then
+if ! [ -f bin/keystore.jks ]; then 
 	keytool -genkeypair -keystore bin/keystore.jks -alias androidKey \
 	-validity 10000 -keyalg RSA -keysize 2048 -storepass android -keypass android	
-fi
+fi && \
 
 apksigner sign --ks bin/keystore.jks --ks-key-alias androidKey --ks-pass pass:android \
 --key-pass pass:android --out bin/apk/build.apk bin/apk/build.align.apk && \
 
 #INSTALL
-PACKAGE=`echo $PACKAGE | tr '/' '.'`
+PACKAGE=`echo $PACKAGE | tr '/' '.'` && \
 
 adb devices && \
 adb install -r bin/apk/build.apk && \
